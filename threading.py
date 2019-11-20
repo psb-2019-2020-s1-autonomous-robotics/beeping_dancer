30fc1fac3cd4d77b97633e5cfbff40d42cacaa68
vwick <35352438+vwick@users.noreply.github.com>
2019-11-20 09:30:13 -0500

    Create threading.py

---
+++ threading.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env pybricks-micropython
+import threading
+
+from pybricks import ev3brick as brick
+from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
+                                 InfraredSensor, UltrasonicSensor, GyroSensor)
+from pybricks.parameters import (Port, Stop, Direction, Button, Color,
+                                 SoundFile, ImageFile, Align)
+from pybricks.tools import print, wait, StopWatch
+from pybricks.robotics import DriveBase
+
+motorB = Motor(Port.B)
+sensor1 = UltrasonicSensor(Port.S1)
+A = 440.000
+G = 381.995
+Fsharp = 369.994
+E = 329.628
+D = 293.665
+C = 261.626
+lowB = 246.942
+
+# Write your program here
+brick.sound.beep()
+songOver = False
+danceOver = False
+
+def sing(Hz = [G, G, Fsharp, Fsharp, lowB, D, lowB, lowB, G, G, Fsharp, Fsharp, lowB, 1, G, G, Fsharp, Fsharp, lowB, D, lowB, D, E, C, D, lowB, 1], d = [1, 1, 1, 1, 1, 0.5, 1.5, 1, 0.5, 1.5, 1, 1, 1, 3, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 3]):
+    #Sings the song entered. Default is Spooky Scary Skeletons
+    for f in range(0,len(Hz)):
+        if Hz[f] == 1:
+            wait(250*d[f])
+        else:
+            brick.sound.beep(Hz[f], 250*d[f])
+        wait(125)
+
+def dance():
+    for a in range(0,4):
+        motorB.run_target(500,360)
+        motorB.run_target(500,0)
+
+d = threading.Thread(target=dance)
+s = threading.Thread(target=sing)
+d.start()
+s.start()
