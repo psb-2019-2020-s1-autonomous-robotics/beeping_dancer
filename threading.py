45958780a8f66d1855492f4b38a2cabd6927eff9
may80914 <57004560+may80914@users.noreply.github.com>
2019-11-25 10:26:54 -0500

    Now threading works!

--- threading.py
+++ threading.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env pybricks-micropython
 #https://en.wikipedia.org/wiki/Piano_key_frequencies
 #https://www.musicnotes.com/images/productimages/large/mtd/MN0168952.gif
+#https://pymotw.com/3/threading/
 
 from pybricks import ev3brick as brick
 from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
@@ -23,8 +24,10 @@ lowB = 246.942
 
 hertz = [G, G, Fsharp, Fsharp, lowB, D, lowB, lowB, G, G, Fsharp, Fsharp, lowB, 1, G, G, Fsharp, Fsharp, lowB, D, lowB, D, E, C, D, lowB, 1]
 duration = [1, 1, 1, 1, 1, 0.5, 1.5, 1, 0.5, 1.5, 1, 1, 1, 3, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 3]
-song_over = 1 
-dance_over = 1
+
+song_on = True
+dance_on = True
+
 def sing():
     #Sings the song entered. Default is Spooky Scary Skeletons
     for f in range(0,len(hertz)):
@@ -33,21 +36,28 @@ def sing():
         else:
             brick.sound.beep(hertz[f], 250*duration[f])
         wait(125)
-    song_over = 0
+    global song_on
+    song_on = False
+    #print("According to sing, which is now over, dance_on = %s and song_on = %s." % (dance_on, song_on))
 
 def dance():
     
     for a in range(0,4):
         motorB.run_target(500,360)
         motorB.run_target(500,0)
-    dance_over = 0
+    global dance_on
+    dance_on = False
+    #print("According to dance, which is now over, dance_on = %s and song_on = %s." % (dance_on, song_on))
 
 s = threading.Thread(name='sing', target=sing)
 d = threading.Thread(name='dance', target=dance)
+while True:
+    if sensor1.distance() < 500:
+        song_on = True
+        dance_on = True
+        d.start()
+        #print("something")
+        s.start()
 
-d.start()
-print("something")
-s.start()
-
-while song_over or dance_over:
-    print(song_over, dance_over)
+        while song_on or dance_on:
+            pass
